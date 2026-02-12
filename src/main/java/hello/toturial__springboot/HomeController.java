aed100e750ac04b7df87de16abf421979f87699b
fyfaris <farizfy0@gmail.com>
2026-02-12 20:10:51 +0700

    add index

--- src/main/java/hello/toturial__springboot/HomeController.java
+++ src/main/java/hello/toturial__springboot/HomeController.java
@@ -1,8 +1,14 @@
 package hello.toturial__springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @GetMapping("/")
+    public String home() {
+        return "index"; // matches index.html in templates folder
+    }
 }
+
